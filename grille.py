123c508d9c2ec75fee00313b4101804788d4f303
ymamou.younais <younais-elmir-w.ymamou@etu.univ-littoral.fr>
2026-06-10 09:53:58 +0200

    Creation de la classe grille

--- grille.py
+++ grille.py
@@ -1,15 +1,32 @@
+#Creation de la classe Grille du jeu Neonaure.
 import json
-def generer_grille_vide(taille=8):
-    grille = []
-    for x in range(taille):
-        for y in range(taille):
-            grille.append([x, y, 0])
-    return {"grille_sudoku": grille}
-donnees = generer_grille_vide(8)
-with open("grille_auto.json", "w") as f:
-    json.dump(donnees, f, indent=4)
-nom_fichier = "grille_sudoku.json"
-with open(nom_fichier, 'w', encoding='utf-8') as fichier_json:
-    print(f" La grille a été exportée dans le fichier : {nom_fichier}")
+from PIL import Image
 
-    
+class Grille:
+    def __init__(self, taille=9):
+        self.taille = taille
+        self.donnees = self.generer_grille_vide()
+
+    def generer_grille_vide(self):
+        grille = []
+        for x in range(self.taille):#boucle pour la ligne 
+            for y in range(self.taille):#boucle pour la cologne 
+                grille.append([x, y, 0])
+        return {"grille_sudoku": grille}
+
+    def charger_image(self):
+        chemin_image = "Exemples de grille-20260609/grille1.png" #chemin relatif du fichier ou hardcoder 
+        #gere l'ouverture du fichier et l'affichage du fichier 
+        try:
+            img = Image.open(chemin_image)
+            img.show()
+            print(f"Format: {img.format}, Taille: {img.size}, Mode: {img.mode}")# optionele donne des info sur l'image 
+            #gestion des erreur envoi  un message d'erreru si il trouve pas le fichier ou si une erreur est ariver 
+        except FileNotFoundError:
+            print(f"Erreur : Le fichier est introuvable à l'emplacement : {chemin_image}")
+        except Exception as e:
+            print(f"Une erreur est survenue : {e}")
+
+# Exemple d'utilisation :
+ma_grille = Grille(9)
+ma_grille.charger_image()
