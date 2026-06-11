713c399aedd9544fb26e9a93aa040cbfa463844d
Selim Trilla <selim.trilla@etudiant.univ-littoral.fr>
2026-06-11 14:55:25 +0200

    Création du Solver : backtracking récursif (méthodes statiques)

---
+++ modele/Solver.py
@@ -0,0 +1,86 @@
+from Grille import Grille
+from Case import Case
+from Validator import Validator
+
+
+class Solver:
+
+    @staticmethod
+    def solve(grid):
+        """
+        Résout la grille par backtracking.
+        Retourne True si la grille est résolue, False sinon.
+        """
+
+        empty_cell = Solver.find_empty_cell(grid)
+
+        if empty_cell is None:
+            return True
+
+        x = empty_cell.get_x()
+        y = empty_cell.get_y()
+
+        for value in range(1, 6):
+            is_valid, _ = Validator.check_move(grid, x, y, value)
+
+            if is_valid:
+
+                empty_cell.set_value(value)
+
+                if Solver.solve(grid):
+                    return True
+
+
+                empty_cell.set_value(0)
+
+        return False
+
+    @staticmethod
+    def find_empty_cell(grid):
+        """
+        Retourne la première case vide (valeur 0) trouvée, ou None.
+        """
+        for case in grid.get_cases():
+            if case.get_value() == 0:
+                return case
+        return None
+
+
+"""
+if __name__ == "__main__":
+
+    from Motif import Motif
+
+    if __name__ == "__main__":
+        print("--- Test du Solver ---")
+
+
+        g = Grille(2, 2)
+
+        m = Motif("M1")
+        m.add_case(g.get_case(0, 0))
+        m.add_case(g.get_case(1, 0))
+        m.add_case(g.get_case(0, 1))
+        m.add_case(g.get_case(1, 1))
+        g.add_motif(m)
+
+        print("Avant résolution :")
+        print(g)
+
+        if Solver.solve(g):
+            print("\nGrille résolue :")
+            print(g)
+        else:
+            print("\nImpossible de résoudre.")
+ """
+if __name__ == "__main__":
+    g = Grille()
+    g.load_json("grille1.json")
+
+    print("Avant résolution :")
+    print(g)
+    if Solver.solve(g):
+        print("\nGrille résolue :")
+        print(g)
+    else:
+        print("\nImpossible de résoudre.")
